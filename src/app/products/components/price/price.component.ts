55b9f4c82f644df40e324dddd6ef79f82af7cdea
lumusitech <lumusitech@gmail.com>
2023-10-22 23:04:55 -0300

    feat: add interval at ngOnInit and then destroy thi at ngOnDestroy

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -6,6 +6,7 @@ import {
   OnInit,
   SimpleChanges,
 } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -15,9 +16,14 @@ import {
 export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   @Input()
   public price: number = 0;
+  private interval$?: Subscription;
 
   ngOnInit(): void {
     console.log('Child Component: ngOnInit');
+
+    this.interval$ = interval(1000).subscribe((value) => {
+      console.log('Tick: ', value);
+    });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -27,5 +33,7 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
 
   ngOnDestroy(): void {
     console.log('Child Component: ngOnDestroy');
+
+    this.interval$?.unsubscribe();
   }
 }
