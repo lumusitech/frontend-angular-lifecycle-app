b77e016bb3710f2b639b2b439b535d27a0f485e5
lumusitech <lumusitech@gmail.com>
2023-10-22 22:46:19 -0300

    feat: add basic lifecycle hooks

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrls: ['./price.component.css'],
 })
-export class PriceComponent {}
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input()
+  public price: number = 0;
+
+  ngOnInit(): void {
+    console.log('Child Component: ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Child Component: ngOnChanges');
+    console.log({ changes });
+  }
+
+  ngOnDestroy(): void {
+    console.log('Child Component: ngOnDestroy');
+  }
+}
